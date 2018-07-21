88251f6524210e8cb92bd3bacc91893717ae74c4
Ted Holmberg <scalemailted@gmail.com>
2018-07-21 18:09:14 -0500

    corrected silly bug with : instead of =

--- studios/studio02/Platformer/scripts/game.js
+++ studios/studio02/Platformer/scripts/game.js
@@ -1,7 +1,7 @@
 var config = new Object();
 
-config.type: Phaser.CANVAS;
-config.pixelArt: true;
+config.type= Phaser.CANVAS;
+config.pixelArt= true;
 config.width = 32 * 24;
 config.height =  32 * 16;
 config.scene = [Level1, Level2 ];
--- studios/studio03/Platformer/scripts/game.js
+++ studios/studio03/Platformer/scripts/game.js
@@ -1,8 +1,8 @@
 var config = new Object();
 
 //config.type = Phaser.CANVAS;
-config.type: Phaser.CANVAS;
-config.pixelArt: true;
+config.type= Phaser.CANVAS;
+config.pixelArt= true;
 config.width = 32 * 24;
 config.height = 32 * 16;
 config.scene = [ Level1, Level2];
